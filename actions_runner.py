1c4d09327035fa65f81600ed238abe05fa9916f2
WOLFIE-OG <oliverjtodd@yahoo.com>
2024-12-06 14:49:20 +0000

    + Update

--- actions_runner.py
+++ actions_runner.py
@@ -6,7 +6,7 @@
 #    By: otodd <otodd@student.42london.com>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/03 11:56:46 by otodd             #+#    #+#              #
-#    Updated: 2024/12/04 23:42:22 by otodd            ###   ########.fr        #
+#    Updated: 2024/12/06 14:43:43 by otodd            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -89,8 +89,8 @@ for workflow in os.listdir(WORKFLOW_DIR):
         action = safe_load(f)
 
     for job in action["jobs"]:
-        env = action["jobs"][job].get("env")
-        env_i = [item for item in action["jobs"][job].get("env").keys()]
+        env = action["jobs"][job].get("env", {})
+        env_i = [item for item in env.keys()]
         logger.debug(f"Running steps for {action.get("name")} on job {job}")
         length = len(action["jobs"][job].get("steps"))
         for index, step in enumerate(action["jobs"][job].get("steps")):
